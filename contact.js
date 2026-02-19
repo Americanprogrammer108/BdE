ae953ac18e41ecd1bbbc03a496e496b4d40009d7
Ethan Chen <chensong108330@gmail.com>
2026-02-18 23:16:16 -0500

    Initial commit

---
+++ contact.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+
+    function getLink()
+    {
+        let link = "https://discord.gg/AGAFkM5Ws";
+        let discord_link = document.getElementById("discord-link");
+        discord_link.innerHTML = link;
+    }
+});
