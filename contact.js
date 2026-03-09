4ca24c6011f5428a308ece95fa5b2ecf4527387e
Ethan Chen <chensong108330@gmail.com>
2026-03-09 15:26:41 -0400

    Updated files

--- contact.js
+++ contact.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    function getLink()
-    {
-        let link = "https://discord.gg/AGAFkM5Ws";
-        let discord_link = document.getElementById("discord-link");
-        discord_link.innerHTML = link;
-    }
+
 });
+
+function getLink()
+{
+    let link = "https://discord.gg/AGAFkM5Ws";
+    let discord_link = document.getElementById("discord-link");
+    discord_link.innerHTML = link;
+}
